662f09fc69423a627e0e638c8fd950e10c5a4e9e
Abu Anwar <abuanwar072@gmail.com>
2021-06-18 15:41:16 +0600

    bug fixed

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFFFF0000);
 const defaultPadding = 16.0;
-const apiKey = "AIzaSyAXrFn4lpaBX8aTceFtZrmPhAsC8N-cCsU";
-const channelId = "UCJm7i4g4z7ZGcJA_HKHLCVw";
+const apiKey = "yourKey";
+const channelId = "channelID";
 
